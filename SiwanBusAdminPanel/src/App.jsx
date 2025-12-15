800259f0ce21683f332eef4dc9077f6eef8ad0fa
Sultan Ashraf <sultanashraf.repo@gmail.com>
2025-12-15 20:16:17 +0530

    feat: implement routing with lazy loading for pages in new routes file

--- SiwanBusAdminPanel/src/App.jsx
+++ SiwanBusAdminPanel/src/App.jsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
+import { useState, lazy } from 'react'
 import { Route, createRoutesFromElements, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import Dashboard from './pages/Dashboard';
-import RootLayout from './layout/RootLayout';
-
-const Dashboard = lazy(() => import('./pages/Dashboard'));
-const Buses = lazy(() => import('./pages/Buses'));
-const Trip = lazy(() => import('./pages/Trips'));
-const Bookings = lazy(() => import('./pages/Bookings'));
-const Users = lazy(() => import('./pages/Users'));
+import router from './routes/routes';
 
 const App = () => {
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RootLayout />} >
-        <Route index path="/" element={<Dashboard />} />
-        <Route path='/buses' element={Buses} />
-      </Route>
-    )
-  )
-
-  return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Dashboard />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router}/>
 }
 
 export default App;
