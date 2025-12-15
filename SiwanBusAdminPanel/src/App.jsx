67be803d389110bd1b3b66361fb6105a6b1a7fb6
Sultan Ashraf <sultanashraf.repo@gmail.com>
2025-12-15 19:52:29 +0530

    feat: refactor App component to use lazy loading for pages and add Buses route

--- SiwanBusAdminPanel/src/App.jsx
+++ SiwanBusAdminPanel/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
-import { BrowserRouter, Router, Routes, Route, createRoutesFromElements, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Route, createRoutesFromElements, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import Dashboard from './pages/Dashboard';
 import RootLayout from './layout/RootLayout';
+
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Buses = lazy(() => import('./pages/Buses'));
+const Trip = lazy(() => import('./pages/Trips'));
+const Bookings = lazy(() => import('./pages/Bookings'));
+const Users = lazy(() => import('./pages/Users'));
 
 const App = () => {
 
@@ -11,6 +17,7 @@ const App = () => {
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />} >
         <Route index path="/" element={<Dashboard />} />
+        <Route path='/buses' element={Buses} />
       </Route>
     )
   )
