7184f15b236eeecb37649ddc09d6adcf4db8bc81
Sultan Ashraf <sultanashraf.repo@gmail.com>
2026-01-27 17:32:37 +0530

    feat: add styles for SearchBox component and update input structure

--- SiwanBusAdminPanel/src/components/SearchBox/index.jsx
+++ SiwanBusAdminPanel/src/components/SearchBox/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBox = () => {
     return(
         <div className="search-box">
-            <input type="text" placeholder="Search..." />
+            {/* <input className="search-input" type="text" placeholder="Search..." /> */}
         </div>
     )
 }
