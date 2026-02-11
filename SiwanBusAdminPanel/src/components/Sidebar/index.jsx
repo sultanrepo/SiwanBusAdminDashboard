6758c3e27418d248f1c94858b66e9202220dd1d5
Sultan Ashraf <sultanashraf.repo@gmail.com>
2026-02-11 10:40:17 +0530

    Side-bar

--- SiwanBusAdminPanel/src/components/Sidebar/index.jsx
+++ SiwanBusAdminPanel/src/components/Sidebar/index.jsx
@@ -3,33 +3,33 @@ import { sidebarConfig } from "../../config/sidebarConfig";
 
 
 const Sidebar = ({ role, collapsed }) => {
-  // return (
-  //   <aside className={`sidebar ${collapsed ? "collapsed" : ""}`}>
-  //     {sidebarConfig.map(section => (
-  //       <div key={section.section} className="sidebar-section">
-  //         {!collapsed && (
-  //           <p className="section-title">{section.section}</p>
-  //         )}
+  return (
+    <aside className={`sidebar ${collapsed ? "collapsed" : ""}`}>
+      {sidebarConfig.map(section => (
+        <div key={section.section} className="sidebar-section">
+          {!collapsed && (
+            <p className="section-title">{section.section}</p>
+          )}
 
-  //         {section.items
-  //           .filter(item => item.roles.includes(role))
-  //           .map(item => {
-  //             const Icon = item.icon;
-  //             return (
-  //               <NavLink
-  //                 key={item.path}
-  //                 to={item.path}
-  //                 className="sidebar-item"
-  //               >
-  //                 <Icon className="icon" />
-  //                 {!collapsed && <span>{item.label}</span>}
-  //               </NavLink>
-  //             );
-  //           })}
-  //       </div>
-  //     ))}
-  //   </aside>
-  // );
+          {section.items
+            .filter(item => item.roles.includes(role))
+            .map(item => {
+              const Icon = item.icon;
+              return (
+                <NavLink
+                  key={item.path}
+                  to={item.path}
+                  className="sidebar-item"
+                >
+                  <Icon className="icon" />
+                  {!collapsed && <span>{item.label}</span>}
+                </NavLink>
+              );
+            })}
+        </div>
+      ))}
+    </aside>
+  );
 };
 
 export default Sidebar;
